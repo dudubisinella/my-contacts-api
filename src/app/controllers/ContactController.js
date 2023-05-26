45071a9ce89a3c025dcbc2a5775b098451525341
bisinella <eduardo.bisinella@rcxit.com.br>
2023-05-26 17:45:18 -0300

    feat: nova rota create contact

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -19,8 +19,23 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {
+  async store(request,response) {
+    const { name, email, phone, category_id } = request.body;
 
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if(contactExists) {
+      return response.status(400).json({error: 'This e-mail is already in use'});
+    }
+
+    const contact = await ContactsRepository.create({
+      name,
+      email,
+      phone,
+      category_id
+    });
+
+    response.json(contact);
   }
 
   update() {
